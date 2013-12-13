ab3f814b4db8bae29bd3cc6eee053dd35f01aec9
Blaine Bublitz <blaine@iceddev.com>
2013-12-13 11:39:33 -0800

    use lodash._reinterpolate to have ES6 delimiters, add more template tests

--- test/main.js
+++ test/main.js
@@ -51,6 +51,7 @@ describe('gulp-util', function() {
       etmpl.should.equal(expected);
       done();
     });
+
     it('should work with a template and data', function(done){
       var opt = {
         name:"todd",
@@ -76,6 +77,51 @@ describe('gulp-util', function() {
         should.exist(err);
         done();
       }
+    });
+
+    it('should ignore modified templateSettings', function(done){
+      var templateSettings = require('lodash.templatesettings');
+      templateSettings.interpolate = /\{\{([\s\S]+?)\}\}/g;
+
+      var opt = {
+        name:"todd",
+        file: {
+          path: "hi.js"
+        }
+      };
+      var expected = "test {{name}} hi.js";
+
+      var tmpl = util.template('test {{name}} <%= file.path %>');
+      should.exist(tmpl);
+      'function'.should.equal(typeof(tmpl));
+
+      // eval it now
+      var etmpl = tmpl(opt);
+      'string'.should.equal(typeof(etmpl));
+      etmpl.should.equal(expected);
+
+      done();
+    });
+
+    it('should allow ES6 delimiters', function(done){
+      var opt = {
+        name:"todd",
+        file: {
+          path: "hi.js"
+        }
+      };
+      var expected = "test todd hi.js";
+
+      var tmpl = util.template('test ${name} ${file.path}');
+      should.exist(tmpl);
+      'function'.should.equal(typeof(tmpl));
+
+      // eval it now
+      var etmpl = tmpl(opt);
+      'string'.should.equal(typeof(etmpl));
+      etmpl.should.equal(expected);
+
+      done();
     });
 
   });
