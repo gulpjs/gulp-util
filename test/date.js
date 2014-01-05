60f78e0ae5a79ffdbcbeae8827890bee251321a2
Ben Briggs <therealbenbriggs@hotmail.com>
2014-01-05 19:11:30 +0000

    Fixes bug in test.

--- test/date.js
+++ test/date.js
@@ -12,7 +12,7 @@ describe('date', function() {
   it('should return today\'s date', function(done) {
     var time = new Date();
     var dateutil = util.date('HH:MM:ss');
-    dateutil.should.equal(time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds());
+    dateutil.should.equal(('0' + time.getHours()).slice(-2) + ':' + ('0' + time.getMinutes()).slice(-2) + ':' + ('0' + time.getSeconds()).slice(-2));
     done();
   })
 });
