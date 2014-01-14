c01a5bf58d8ea9eb1537ec8912856cae5e86ce99
Contra <contra@maricopa.edu>
2014-01-14 02:35:41 -0700

    test for pipeline

--- test/combine.js
+++ test/combine.js
@@ -24,4 +24,15 @@ describe('combine()', function(){
     var factory = util.combine(inp, inp2);
     factory().should.be.instanceof(Stream);
   });
+  it('should return a function that returns a stream combination when given an array', function(done){
+    var src = [1,2,3];
+    var inp = es.readArray(src);
+    var inp2 = es.writeArray(function(err, data){
+      should.not.exist(err);
+      data.should.eql(src);
+      done();
+    });
+    var factory = util.combine([inp, inp2]);
+    factory().should.be.instanceof(Stream);
+  });
 });
