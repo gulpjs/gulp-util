0c4f90f529d7c366ca67ae533b6517c14ceea807
Javier Carrillo <stringparser@gmail.com>
2015-04-12 21:03:36 +0200

    fix tests logging

--- test/log.js
+++ test/log.js
@@ -4,30 +4,38 @@ require('mocha');
 
 describe('log()', function(){
   it('should work i guess', function(done){
-    var writtenValue;
+    var count = 0;
+    var writtenValue = '';
 
     // Stub process.stdout.write
     var stdout_write = process.stdout.write;
     process.stdout.write = function(value) {
-      writtenValue = value;
+      writtenValue += value;
+      if(++count > 2){
+        // Restore process.stdout.write after test
+        process.stdout.write = stdout_write;
+      }
     };
 
     util.log(1, 2, 3, 4, 'five');
     var time = util.date(new Date(), 'HH:MM:ss');
     writtenValue.should.eql('[' + util.colors.grey(time) + '] 1 2 3 4 five\n');
 
-    // Restore process.stdout.write
-    process.stdout.write = stdout_write;
     done();
   });
 
   it('should accept formatting', function(done){
-    var writtenValue;
+    var count = 0;
+    var writtenValue = '';
 
     // Stub process.stdout.write
     var stdout_write = process.stdout.write;
     process.stdout.write = function(value) {
-      writtenValue = value;
+      writtenValue += value;
+      if(++count > 2){
+        // Restore process.stdout.write after test
+        process.stdout.write = stdout_write;
+      }
     };
 
     util.log('%s %d %j', 'something', 0.1, {key: 'value'});
@@ -36,8 +44,6 @@ describe('log()', function(){
       '[' + time + '] something 0.1 {\"key\": \"value\"}\n'
     );
 
-    // Restore process.stdout.write
-    process.stdout.write = stdout_write;
     done();
   });
 });
